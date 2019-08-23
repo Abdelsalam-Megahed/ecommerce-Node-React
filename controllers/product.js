71fdb2b6bb10e91427963441a97bc1d9f383ccdd
Abdelsalam-Megahed <47678171+Abdelsalam-Megahed@users.noreply.github.com>
2019-08-23 15:20:33 +0300

    Product delete, product update.

--- controllers/product.js
+++ controllers/product.js
@@ -63,4 +63,63 @@ exports.read = (req, res) => {
     return res.json(req.product);
 }
 
+exports.remove = (req, res) => {
+    let product = req.product;
+    product.remove((error, productDeleted) => {
+        if(error){
+            return res.status(400).json({
+                error: "Product not found"
+        })
+    }
+    
+        res.json({
+            productDeleted,
+            message: "Product has been deleted"
+        })
+    })
 
+
+}
+
+exports.update = (req, res) => {
+    let form = new formidable.IncomingForm();
+    form.keepExtensions = true;
+    form.parse(req, (err, fields, files) => {
+        if (err) {
+            return res.status(400).json({
+                error: "Image could not be uploaded"
+            });
+        }
+        const {name, description, price, category, quantity, shipping} = fields;
+
+        if(!name || !description || !price || !category || !quantity || !shipping){
+            return res.status(400).json({
+                error: "all the fields must be added"
+            })
+        }
+
+        let product = req.product;
+        product = _.extend(product, fields)
+
+        if (files.photo) {
+            // console.log(files.photo);
+            if(files.photo.size > 5000000){
+                return res.status(400).json({
+                    error: 'Image should be less than 5 MBs'
+                })
+            }
+            product.photo.data = fs.readFileSync(files.photo.path);
+            product.photo.contentType = files.photo.type;
+        }
+
+        product.save((err, result) => {
+            if (err) {
+                return res.status(400).json({
+                    error: errorHandler(err)
+                });
+            }
+            res.json(result);
+        });
+    });
+
+}
