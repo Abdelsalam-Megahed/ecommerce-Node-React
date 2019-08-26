c8ff98d86c66715dc03c7f3ef167e2205f38d683
Abdelsalam-Megahed <47678171+Abdelsalam-Megahed@users.noreply.github.com>
2019-08-26 22:05:49 +0300

    User profile read and update.

--- controllers/user.js
+++ controllers/user.js
@@ -12,3 +12,31 @@ exports.userById = (req, res, next, id) => {
     })
 
 }
+
+exports.read = (req, res) => {
+    req.profile.hashed_password = undefined;
+    req.profile.salt = undefined;
+
+    return res.json({
+        userProfile: req.profile
+    })
+}
+
+exports.update = (req, res) => {
+    User.findOneAndUpdate(
+        {_id: req.profile._id}, 
+        {$set: req.body}, 
+        {new: true},
+        (err, user) => {
+            if(err || !user){
+                return res.status(400).json({
+                    error: 'Update failed.'
+                })
+            }
+            user.hashed_password= undefined;
+            user.salt = undefined;
+
+            return res.json(user)
+        }
+        )    
+}
