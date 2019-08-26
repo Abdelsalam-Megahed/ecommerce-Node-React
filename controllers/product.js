136a9eaccc0fe6caea12a01b78c7ca5c275e9d8f
Abdelsalam-Megahed <47678171+Abdelsalam-Megahed@users.noreply.github.com>
2019-08-26 17:48:37 +0300

    Related products.

--- controllers/product.js
+++ controllers/product.js
@@ -4,6 +4,23 @@ const fs = require("fs");
 const Product = require("../models/product");
 const { errorHandler } = require("../helpers/dbErrorHandler");
 
+exports.productById = (req, res, next, id) => {
+    Product.findById(id).exec((err, product) => {
+        if (err || !product) {
+            return res.status(400).json({
+                error: "Product not found"
+            });
+        }
+        req.product = product;
+        next();
+    });
+};
+
+exports.read = (req, res) => {
+    req.product.photo = undefined;
+    return res.json(req.product);
+};
+
 exports.create = (req, res) => {
     let form = new formidable.IncomingForm();
     form.keepExtensions = true;
@@ -13,22 +30,40 @@ exports.create = (req, res) => {
                 error: "Image could not be uploaded"
             });
         }
-        const {name, description, price, category, quantity, shipping} = fields;
+        // check for all fields
+        const {
+            name,
+            description,
+            price,
+            category,
+            quantity,
+            shipping
+        } = fields;
 
-        if(!name || !description || !price || !category || !quantity || !shipping){
+        if (
+            !name ||
+            !description ||
+            !price ||
+            !category ||
+            !quantity ||
+            !shipping
+        ) {
             return res.status(400).json({
-                error: "all the fields must be added"
-            })
+                error: "All fields are required"
+            });
         }
 
         let product = new Product(fields);
 
+        // 1kb = 1000
+        // 1mb = 1000000
+
         if (files.photo) {
-            // console.log(files.photo);
-            if(files.photo.size > 5000000){
+            // console.log("FILES PHOTO: ", files.photo);
+            if (files.photo.size > 1000000) {
                 return res.status(400).json({
-                    error: 'Image should be less than 5 MBs'
-                })
+                    error: "Image should be less than 1mb in size"
+                });
             }
             product.photo.data = fs.readFileSync(files.photo.path);
             product.photo.contentType = files.photo.type;
@@ -45,41 +80,19 @@ exports.create = (req, res) => {
     });
 };
 
-exports.productById = (req, res, next, id) => {
-    Product.findById(id).exec((error, product) => {
-        if(error || !product){
-            return res.status(400).json({
-                  error: "Product not found"  
-            })
-        }
-        req.product = product;
-        next();
-    })
-}
-
-
-exports.read = (req, res) => {
-    req.product.photo = undefined;
-    return res.json(req.product);
-}
-
 exports.remove = (req, res) => {
     let product = req.product;
-    product.remove((error, productDeleted) => {
-        if(error){
+    product.remove((err, deletedProduct) => {
+        if (err) {
             return res.status(400).json({
-                error: "Product not found"
-        })
-    }
-    
+                error: errorHandler(err)
+            });
+        }
         res.json({
-            productDeleted,
-            message: "Product has been deleted"
-        })
-    })
-
-
-}
+            message: "Product deleted successfully"
+        });
+    });
+};
 
 exports.update = (req, res) => {
     let form = new formidable.IncomingForm();
@@ -90,23 +103,41 @@ exports.update = (req, res) => {
                 error: "Image could not be uploaded"
             });
         }
-        const {name, description, price, category, quantity, shipping} = fields;
+        // check for all fields
+        const {
+            name,
+            description,
+            price,
+            category,
+            quantity,
+            shipping
+        } = fields;
 
-        if(!name || !description || !price || !category || !quantity || !shipping){
+        if (
+            !name ||
+            !description ||
+            !price ||
+            !category ||
+            !quantity ||
+            !shipping
+        ) {
             return res.status(400).json({
-                error: "all the fields must be added"
-            })
+                error: "All fields are required"
+            });
         }
 
         let product = req.product;
-        product = _.extend(product, fields)
+        product = _.extend(product, fields);
+
+        // 1kb = 1000
+        // 1mb = 1000000
 
         if (files.photo) {
-            // console.log(files.photo);
-            if(files.photo.size > 5000000){
+            // console.log("FILES PHOTO: ", files.photo);
+            if (files.photo.size > 1000000) {
                 return res.status(400).json({
-                    error: 'Image should be less than 5 MBs'
-                })
+                    error: "Image should be less than 1mb in size"
+                });
             }
             product.photo.data = fs.readFileSync(files.photo.path);
             product.photo.contentType = files.photo.type;
@@ -121,26 +152,63 @@ exports.update = (req, res) => {
             res.json(result);
         });
     });
+};
 
-}
+/**
+ * sell / arrival
+ * by sell = /products?sortBy=sold&order=desc&limit=4
+ * by arrival = /products?sortBy=createdAt&order=desc&limit=4
+ * if no params are sent, then all products are returned
+ */
 
 exports.list = (req, res) => {
-  let order = req.query.order ? req.query.order : 'asc'
-  let sortBy = req.query.sortBy ? req.query.sortBy : '_id'
-  let limit = req.query.limit ? req.query.limit : 6
+    let order = req.query.order ? req.query.order : "asc";
+    let sortBy = req.query.sortBy ? req.query.sortBy : "_id";
+    let limit = req.query.limit ? parseInt(req.query.limit) : 6;
 
-  Product.find()
+    Product.find()
         .select("-photo")
-        .populate('category')
+        .populate("category")
         .sort([[sortBy, order]])
         .limit(limit)
         .exec((err, products) => {
             if (err) {
                 return res.status(400).json({
-                    error: errorHandler(err)
+                    error: "Products not found"
                 });
             }
-            return res.send(products)
-        })
+            res.json(products);
+        });
+};
 
+/**
+ * it will find the products based on the req product category
+ * other products that has the same category, will be returned
+ */
+
+exports.listRelated = (req, res) => {
+    let limit = req.query.limit ? parseInt(req.query.limit) : 6;
+
+    Product.find({ _id: { $ne: req.product }, category: req.product.category })
+        .limit(limit)
+        .populate("category", "_id name")
+        .exec((err, products) => {
+            if (err) {
+                return res.status(400).json({
+                    error: "Products not found"
+                });
+            }
+            res.json(products);
+        });
+};
+
+exports.listCategories = (req, res) => {
+    Product.distinct("category", {}, (err, categories) => {
+        if (err) {
+            return res.status(400).json({
+                error: "Categories not found"
+            });
+        }
+        return res.json(categories)
+    })
 }
